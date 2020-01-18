3024524ce470e8f4c92c7873dc6ea801ed53bfc2
Alban <alban.xhaferllari@gmail.com>
2020-01-18 16:59:05 -0600

    user password hashed and compared wiht bcrypt

--- models/user.js
+++ models/user.js
@@ -1,16 +1,31 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+
 const Schema = mongoose.Schema;
 
+const SALT_ROUNDS = 6;
+
 const userSchema = new Schema({
-    url: String,
-    classification: [String],
-    confidence: [Number],
-    classIndex: {
-        type: Number,
-        default: 0
-    }
+    name: String,
+    email: { type: String, required: true, lowercase: true, unique: true },
+    password: String
 }, {
     timestamps: true
 });
+
+userSchema.pre('save', function (next) {
+    const user = this;
+    if (!user.isModified('password')) return next();
+    // if user changed then hash the password
+    bcrypt.hash(user.password, SALT_ROUNDS, function (err, hash) {
+        if (err) return next(err);
+        user.password = hash;
+        next();
+    });
+});
+
+userSchema.methods.comparePassword = function (tryPassword, cb) {
+    bcrypt.compare(tryPassword, this.password, cb);
+};
 
 module.exports = mongoose.model('User', userSchema);
