78eb1f8c9c5cd143690f7b92f1633a610d44dc44
Alban <alban.xhaferllari@gmail.com>
2020-02-02 14:39:31 -0600

    mobile css adjustments

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,13 +1,14 @@
 
 import React from 'react'
 import { Link } from 'react-router-dom'
+import './NavBar.css';
 
 const NavBar = (props) => {
 
-    return (<nav className="navbar navbar-dark bg-primary">
+    return (<nav className="navbar navbar-dark bg-primary navigation-main">
         <Link to='/' className='btn btn-primary navbar-brand'>PackingPlanner</Link>
         {(props.user) ?
-            <div>
+            <div className='nav-bar-links'>
                 <Link to='/dashboard' className='btn btn-primary'>Dashboard</Link>
                 <Link to='/trips' className='btn btn-primary'>Trips</Link>
                 <Link to='/closet' className='btn btn-primary'>Closet</Link>
